b99d416f9145de6761f50227a2034f0be34da3ae
Behrad Ravanbod <99behi@gmail.com>
2019-08-19 15:34:08 +0430

    Some changes in binding try/except messages

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import sys
 import socket
+import errno
 from config import config as conf
 from config import messages as msg
 
@@ -10,9 +11,13 @@ def bind_server(host, port):
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server.bind((host, port))
         print(msg.successful_binding)
-    except socket.error:
+        print(msg.host + host)
+        print(msg.port + str(port))
+
+    except socket.error as error_msg:
         print(msg.problem_in_binding + msg.triple_dots)
         print(msg.exiting + msg.triple_dots)
+        print(error_msg)
         sys.exit(0)
 
 

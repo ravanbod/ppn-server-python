fc9c00eef3ab70fe4921f7057bb708660faa0a74
Behrad Ravanbod <99behi@gmail.com>
2019-08-19 22:00:17 +0430

    Multi clients feature was added.

--- config/config.py
+++ config/config.py
@@ -1,5 +1,5 @@
 # --- Server Variables START ---
 HOST = '127.0.0.1'
-PORT = 12345
+PORT = 25087
 MAX_CLIENT = 1000  # Set this -1 if you want unlimited clients.
 # --- Server Variables END ---
--- run.py
+++ run.py
@@ -5,6 +5,41 @@ from config import config as conf
 from config import messages as msg
 
 
+class Client:
+    client = None
+    address = None
+    id = None
+
+    # You can add any variable which you want here
+
+    def __init__(self, c, addr):
+        self.client = c
+        self.address = addr
+        print(msg.new_connection + str(addr))
+        threading.Thread(target=self.run, args=[]).start()
+
+    def run(self):
+        while True:
+            try:
+                data = self.client.recv(1024)  # This message is sent from client
+                if not data:
+                    self.client.close()
+                else:
+                    print(data.decode())
+                    for x in clients:
+                        x.send(data)
+            except socket.error as error_msg:
+                self.client.close()
+                for index, x in enumerate(clients):
+                    if x.address == self.address:
+                        clients.pop(index)
+                print(msg.close_connection + str(self.address))
+                break
+
+    def send(self, data):
+        self.client.send(data)  # this syntax sends msg to client
+
+
 def run_server(host, port):
     global server
     try:
@@ -25,9 +60,13 @@ def run_server(host, port):
 
 server = None
 clients = []
-run_server(conf.HOST, conf.PORT)
 
-while True:
-    if conf.MAX_CLIENT == -1 or len(clients) <= conf.MAX_CLIENT:
-        client, address = server.accept()
+if __name__ == "__main__":
+    run_server(conf.HOST, conf.PORT)
 
+    while True:
+        if conf.MAX_CLIENT == -1 or len(clients) <= conf.MAX_CLIENT:
+            c, a = server.accept()  # A new client connected.
+            clients.append(Client(c, a))  # Add new client to clients list.
+        else:
+            print(msg.max_client + msg.triple_dots)
